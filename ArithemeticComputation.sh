d0a9e81f31b63616dd5111d4149c3a0bd8095e1a
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-26 15:37:22 +0530

    sorting the values in ascending order

--- ArithemeticComputation.sh
+++ ArithemeticComputation.sh
@@ -3,10 +3,12 @@ echo "Arithmetic Computaion and Sorting"
 
 declare -A arithmeticOperation
 
+#Taking inputs from the user
 read -p "Enter first input: " a
 read -p "Enter second input: " b
 read -p "Enter third input: " c
 
+#Evaluating the expressions
 result1=$(($a+$b*$c))
 echo "Result1: "$result1
 result2=$(($a*$b+$c))
@@ -16,13 +18,13 @@ echo "Result3: "$result3
 result4=$(($a%$b+$c))
 echo "Result4: "$result4
 
-
+#Storing the result in Dictionary
 arithmeticOperation[result1]=$result1
 arithmeticOperation[result2]=$result2
 arithmeticOperation[result3]=$result3
 arithmeticOperation[result4]=$result4
 
-
+#Read the Dictionary value in Array
 for((i=0; i<${#arithmeticOperation[@]}; i++))
 do
 	arr[i]=${arithmeticOperation[result$((i+1))]}
@@ -30,5 +32,12 @@ done
 
 echo "Array of dictionary values are: "${arr[@]}
 
+
+#Sorting the result in Descending order
 arr1=($(echo ${arr[@]} | tr " " "\n" | sort -nr))
 echo "Values in Descending order: "${arr1[@]}
+
+
+#Sorting the result in Ascending order
+arr2=($(echo ${arr[@]} | tr " " "\n" | sort -n))
+echo "Values in Ascending order: "${arr2[@]}
