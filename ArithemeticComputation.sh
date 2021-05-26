9b02489e983894cd6e872cbe637db383b9bbbe3b
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-26 15:08:12 +0530

    adding result to the dictionary

--- ArithemeticComputation.sh
+++ ArithemeticComputation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 echo "Arithmetic Computaion and Sorting"
 
+declare -A arithmeticOperation
+
 read -p "Enter first input: " a
 read -p "Enter second input: " b
 read -p "Enter third input: " c
@@ -13,3 +15,9 @@ result3=$(($c+$a/$b))
 echo "Result3: "$result3
 result4=$(($a%$b+$c))
 echo "Result4: "$result4
+
+
+arithmeticOperation[result1]=$result1
+arithmeticOperation[result2]=$result2
+arithmeticOperation[result3]=$result3
+arithmeticOperation[result4]=$result4
